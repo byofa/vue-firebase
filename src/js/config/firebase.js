04fa2b09bf10f1e330c57d4f000ad6d3910bb576
Machado Fabio <machad_f@etna-alternance.net>
2022-07-11 21:00:50 +0200

    update firebase settings

--- src/js/config/firebase.js
+++ src/js/config/firebase.js
@@ -7,7 +7,6 @@ const firebaseConfig = {
   storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.VUE_APP_MESSAGEING_SENDER_ID,
   appId: process.env.VUE_APP_APP_ID,
-  measurementId: process.env.VUE_APP_MEASUREMENT_ID,
 };
 
 initializeApp(firebaseConfig);
